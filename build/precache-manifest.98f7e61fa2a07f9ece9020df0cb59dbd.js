ec3d564b31733744a9b6311d6da94c0f2b21d9c0
Anthony Gaff <anthony.gaff@valid.com>
2020-08-22 18:17:51 -0400

    Create gh-pages branch with static content

---
+++ build/precache-manifest.98f7e61fa2a07f9ece9020df0cb59dbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a367a79ad429e1bf8579fad20385f04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c786455d55d66b72ced9",
+    "url": "/static/css/2.9b79e585.chunk.css"
+  },
+  {
+    "revision": "74f896f88963aed865d0",
+    "url": "/static/css/main.605f9b19.chunk.css"
+  },
+  {
+    "revision": "c786455d55d66b72ced9",
+    "url": "/static/js/2.fdc3a39c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.fdc3a39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f896f88963aed865d0",
+    "url": "/static/js/main.8f25e154.chunk.js"
+  },
+  {
+    "revision": "c2113bfff3544afdddd3",
+    "url": "/static/js/runtime-main.4b7e6b30.js"
+  }
+]);
