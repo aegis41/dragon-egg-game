3295c9493b7ba8c23b224bfb75150f509613b053
Anthony Gaff <anthony.gaff@valid.com>
2020-08-22 22:20:08 -0400

    cleanup finally done?

--- src/components/EggImage.js
+++ src/components/EggImage.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import egg from '../assets/egg00.png';
 import Image from 'react-bootstrap/Image';
 
 class EggImage extends React.Component {
-    eggContext = "../assets/";
+    getEggSource = (egg) => {
+        return "../assets/" + egg + ".png";
+    }
 
     render() {
         return (
-            <Image className="egg-image" src={egg} alt="Plain Egg" fluid />
+            <Image className="egg-image" src={this.getEggSource(this.props.egg)} alt="Plain Egg" fluid />
         );
     };
 };
